ef781ac965eeed7a6ba28bc8e1e9bf86e6cfe70a
DylanT80 <DylanTTran80@gmail.com>
2023-11-05 22:04:13 -0800

    Added search feature, fix alignment in future

--- front-end/pages/home/script.js
+++ front-end/pages/home/script.js
@@ -2,37 +2,40 @@ const pokedex = document.querySelector("#pokedex");
 nextURL = null;
 prevURL = null;
 
-// Fills the pokedex w/ pagination
-// @params
-//      id: unique identifier
+// Fills the pokedex w/ pokemon info
+// @param id - unique identifier
 const fillPokedex = async (id) => {
-    try {       
+    try {
         const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);    // Get pagination data
         pokedex.innerHTML = "";                                                         // Clear the UL
-        
-        // Get individual pokemon data in order
-        const promises = [];
-        response.data.results.forEach(pokemon => {
-            promises.push(getPokemon(pokemon.name));                                    
-        });
-        Promise.all(promises).then((pokemons) => {                                         // Promise.all gurantees in order
-            pokemons.forEach((pokemon) => addPokemon(pokemon));
-        });
-        
-        // Set the nav endpoints
-        nextURL = response.data.next ? "?" + (response.data.next).split("?")[1] : null;
-        prevURL = response.data.previous ? "?" + (response.data.previous).split("?")[1] : null;
+
+        // Specific search
+        if (!id.includes("?")) {
+            addPokemon(response.data);
+        } else {
+            // Get individual pokemon data in order
+            const promises = [];
+            response.data.results.forEach(pokemon => {
+                promises.push(getPokemon(pokemon.name));                                    
+            });
+            Promise.all(promises).then((pokemons) => {                                         // Promise.all gurantees in order
+                pokemons.forEach((pokemon) => addPokemon(pokemon));
+            });
+            
+            // Set the nav endpoints
+            nextURL = response.data.next ? "?" + (response.data.next).split("?")[1] : null;
+            prevURL = response.data.previous ? "?" + (response.data.previous).split("?")[1] : null;
+            }
     } catch (error) {
         console.error(error);
     }
 }
 
 // Add pokemon to pokedex UL
-// @param
-//      pokemon: data of the pokemon
+// @param pokemon - data of the pokemon
 const addPokemon = (pokemon) => {
     const li = document.createElement("li");
-    pokemon.name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+    pokemon.name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);   // Style name
     const pokemonHTMLString = `
     <div class="container">
         <div class="bg-img">
@@ -46,10 +49,8 @@ const addPokemon = (pokemon) => {
 }
 
 // Get individual pokemon info
-// @params
-//      id: unique identifier for pokemon
-// @return
-//      json: a pokemon's data
+// @params id   - unique identifier for pokemon
+// @return json - a pokemon's data
 const getPokemon = async (id) => {
     try {       
         const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
@@ -58,6 +59,21 @@ const getPokemon = async (id) => {
         console.error(error);
     }
 }
+
+const searchPokemon = async (e) => {
+    try {
+        await fillPokedex(e.text);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const input = document.querySelector("#searchbar");
+input.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        fillPokedex(event.target.value);
+    }
+});
 
 // Nav buttons
 const prev = () => {
@@ -70,6 +86,7 @@ const next = () => {
         fillPokedex(nextURL);
     }
 }
+
 
 // Initial pokedex load
 fillPokedex("?limit=24");
